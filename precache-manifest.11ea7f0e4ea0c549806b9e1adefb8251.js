84e84e91b5cea287d42fd9c3684a0c49afc3fa49
Ben <ben@Bens-MacBook-Pro.local>
2019-11-20 17:10:58 +0000

    Updates

---
+++ precache-manifest.11ea7f0e4ea0c549806b9e1adefb8251.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffecda2bccb25eab23a93c693ce3c455",
+    "url": "/match-up/index.html"
+  },
+  {
+    "revision": "c326322c7e7c1cd735ad",
+    "url": "/match-up/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d7c436a59346cbbc9e75",
+    "url": "/match-up/static/js/2.029dc657.chunk.js"
+  },
+  {
+    "revision": "c326322c7e7c1cd735ad",
+    "url": "/match-up/static/js/main.180bd80d.chunk.js"
+  },
+  {
+    "revision": "d639ee4121864d2f93c4",
+    "url": "/match-up/static/js/runtime-main.22c6b2e5.js"
+  },
+  {
+    "revision": "457556db2f1c7716bbf7781e84462a32",
+    "url": "/match-up/static/media/Stu.457556db.jpg"
+  },
+  {
+    "revision": "761ed6d3328ed8b4738639491ed73846",
+    "url": "/match-up/static/media/andy.761ed6d3.jpg"
+  },
+  {
+    "revision": "b6f6971a4f980d45989a8ff46f9c78b7",
+    "url": "/match-up/static/media/dan.b6f6971a.jpg"
+  },
+  {
+    "revision": "64daaae8d2bd4a2bf998d174c42dd5ec",
+    "url": "/match-up/static/media/dan2.64daaae8.jpg"
+  },
+  {
+    "revision": "f919335f799666e9da57a066c0f8a3bd",
+    "url": "/match-up/static/media/dave.f919335f.jpg"
+  },
+  {
+    "revision": "2d1859a435bea93faca81abc25b8577a",
+    "url": "/match-up/static/media/leon.2d1859a4.jpg"
+  },
+  {
+    "revision": "f01fe2a72ee48e9c269191684cf62f62",
+    "url": "/match-up/static/media/liam.f01fe2a7.jpg"
+  },
+  {
+    "revision": "738bce8fcc3f58c3872da20cf75b31c4",
+    "url": "/match-up/static/media/logo.738bce8f.png"
+  },
+  {
+    "revision": "f9e34f9236442a0604dc80460b5a1372",
+    "url": "/match-up/static/media/rach.f9e34f92.jpg"
+  }
+]);
