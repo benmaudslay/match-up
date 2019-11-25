42422389d4994de888b2ba283a0521cbe710fcd1
Ben <ben@Bens-MacBook-Pro.local>
2019-11-25 11:20:08 +0000

    Reset button updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react"
 import styled from "styled-components"
 import Card from "./Card"
+import { Reset } from "./Reset"
 
 import Dan from "./assets/dan.jpg"
 import Dan2 from "./assets/dan2.jpg"
@@ -13,77 +14,7 @@ import Andy from "./assets/andy.jpg"
 import Sid from "./assets/sid.jpeg"
 import pengTing from "./assets/ok.png"
 import github from "./assets/github.png"
-
-const Wrapper = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 150px;
-  gap: 20px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-`
-
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  gap: 15px 15px;
-  width: 660px;
-`
-const MenuButton = styled.button`
-  color: ${props => (props.alt ? "black" : "#fff")};
-  font-size: 24px;
-  height: 150px;
-  width: 150px;
-  background-color: ${props => (props.alt ? "#fff" : "#f7aa3a")};
-  border: ${props => (props.alt ? "5px #1d1e21 solid" : "none")};
-  border-radius: 10%;
-  padding: 5px 20px;
-  margin-bottom: 15px;
-  font-family: Gilroy;
-  /* Center content */
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`
-
-const EndGameReset = styled(MenuButton)`
-  position: unset;
-  top: 0;
-  left: 0;
-  transform: none;
-  margin-top: 20px;
-`
-
-const Message = styled.div`
-  position: absolute;
-  top: 30%;
-  left: 50%;
-  transform: translate(-50%, 0);
-  font-size: 24px;
-  font-weight: 700;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: fit-content;
-`
-
-const MessageImage = styled.img`
-  height: 96px;
-  width: 96px;
-  padding: 10px;
-`
-
-const shuffle = cardDeck => {
-  for (let i = cardDeck.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1))
-    let temp = cardDeck[i]
-    cardDeck[i] = cardDeck[j]
-    cardDeck[j] = temp
-  }
-
-  return cardDeck
-}
+import reset from "./assets/reset.png"
 
 class App extends Component {
   state = {
@@ -175,10 +106,10 @@ class App extends Component {
         {count === cards.length / 2 ? (
           <>
             <Message>
-              <MessageImage src={pengTing} alt="Sweet" />
+              {/* <MessageImage src={pengTing} alt="Sweet" /> */}
               <>You won, congrats kid</>
               <EndGameReset onClick={() => this.handleReset()}>
-                Reset
+                <Reset image={reset} />
               </EndGameReset>
             </Message>
           </>
@@ -202,7 +133,9 @@ class App extends Component {
                   <img height="80" width="80" src={github} alt="Github" />
                 </MenuButton>
               </a>
-              <MenuButton onClick={() => this.handleReset()}>Reset</MenuButton>
+              <MenuButton onClick={() => this.handleReset()}>
+                <Reset image={reset} />
+              </MenuButton>
             </div>
           </Wrapper>
         )}
@@ -212,3 +145,75 @@ class App extends Component {
 }
 
 export default App
+
+const shuffle = cardDeck => {
+  for (let i = cardDeck.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    let temp = cardDeck[i]
+    cardDeck[i] = cardDeck[j]
+    cardDeck[j] = temp
+  }
+
+  return cardDeck
+}
+
+// Styled Components
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 150px;
+  gap: 20px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
+
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  gap: 15px 15px;
+  width: 660px;
+`
+const MenuButton = styled.button`
+  color: ${props => (props.alt ? "black" : "#fff")};
+  font-size: 24px;
+  height: 150px;
+  width: 150px;
+  background-color: ${props => (props.alt ? "#fff" : "#f7aa3a")};
+  border: ${props => (props.alt ? "5px #1d1e21 solid" : "none")};
+  border-radius: 10%;
+  padding: 5px 20px;
+  margin-bottom: 15px;
+  font-family: Gilroy;
+  /* Center content */
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const EndGameReset = styled(MenuButton)`
+  position: unset;
+  top: 0;
+  left: 0;
+  transform: none;
+  margin-top: 20px;
+`
+
+const Message = styled.div`
+  position: absolute;
+  top: 30%;
+  left: 50%;
+  transform: translate(-50%, 0);
+  font-size: 24px;
+  font-weight: 700;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: fit-content;
+`
+
+const MessageImage = styled.img`
+  height: 96px;
+  width: 96px;
+  padding: 10px;
+`
---
+++ src/Reset.js
@@ -0,0 +1,2 @@
+import React from "react"
+export const Reset = ({ image }) => <img src={image} alt="Reset" />
